d98b9363fcc843651d24d191c80ca826511cb698
zasidkayuliia <yuliia.zasidka.ri.2024@lpnu.ua>
2024-09-17 00:25:46 +0300

    first master commit

---
+++ LR 2.1/LR 2.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+double ctg(double x) {
+	return 1 / tan(x);// формула для обчислення ctg
+}
+
+int main()
+{
+	double Pi = 4 * atan(1.); // число пі
+	double alpha; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+
+	cout << "alpha = "; cin >> alpha; // Введення значення альфа
+
+	z1 = sin(Pi / 2 + 3 * alpha) / (1 - sin(3 * alpha - Pi));
+	z2 = ctg(5 * Pi / 4 + 3 * alpha / 2);
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}

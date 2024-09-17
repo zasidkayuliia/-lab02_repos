e31cf8b57be5fe0d76a591825836460d8f0af1cd
zasidkayuliia <yuliia.zasidka.ri.2024@lpnu.ua>
2024-09-17 17:10:35 +0300

    2

--- LR 2.1/LR 2.1.cpp
+++ LR 2.1/LR 2.1.cpp
@@ -10,16 +10,16 @@ int main()
 {
 	double Pi = 4 * atan(1.); // число пі
 	double alpha; // вхідний параметр
-	double z1; // результат обчислення 1-го виразу
+	//double z1; // результат обчислення 1-го виразу
 	double z2; // результат обчислення 2-го виразу
 
 	cout << "alpha = "; cin >> alpha; // Введення значення альфа
 
-	z1 = sin(Pi / 2 + 3 * alpha) / (1 - sin(3 * alpha - Pi));
+	//z1 = sin(Pi / 2 + 3 * alpha) / (1 - sin(3 * alpha - Pi));
 	z2 = ctg(5 * Pi / 4 + 3 * alpha / 2);
 
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
+	//cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
 
 	cin.get();
